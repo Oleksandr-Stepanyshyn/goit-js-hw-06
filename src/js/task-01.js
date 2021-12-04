04a4582c65672d4f5d29f0c52d090d0c1bb6dc71
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-04 19:53:00 +0200

    task1

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const arrCategories = document.querySelectorAll(".item");
+console.log('Number of categories:', arrCategories.length);
+
+console.log(arrCategories);
+const listCategories = document.querySelector(".item h2");
+console.log(listCategories);
+const title = listCategories.textContent;
+console.log(title);

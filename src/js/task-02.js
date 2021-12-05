09db8853e6973e2fb0abd795b9519ece2a378043
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-05 12:56:11 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const markup = [];
+
+function createMarkup (el) {
+  const item = document.createElement('li');
+  item.textContent = el;
+  // console.log(item.textContent);
+  item.classList.add("item")
+  // console.log(item);
+  markup.push(item);
+  return item;
+};
+
+ingredients.forEach(createMarkup);
+ 
+console.log(markup);
+
+const list = document.querySelector('#ingredients')
+// console.log(list);
+list.append(markup.join(''));
+
+
+
+
+

aba192a596d17a2aa696f9ffc5395450f347e423
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-05 11:25:03 +0200

    task-01-finish

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,8 +1,16 @@
-const arrCategories = document.querySelectorAll(".item");
-console.log('Number of categories:', arrCategories.length);
+const allCategoriesEl = document.querySelectorAll(".item");
+console.log('Number of categories:', allCategoriesEl.length);
 
-console.log(arrCategories);
-const listCategories = document.querySelector(".item h2");
-console.log(listCategories);
-const title = listCategories.textContent;
-console.log(title);
+// console.log(arrCategories);
+// const listCategoriesEl = document.querySelectorAll(".item");
+// console.log(listCategoriesEl);
+// const titleCategory = listCategoriesEl[0].firstElementChild.textContent;
+// console.log('Category:', titleCategory);
+// const itemsCategotyEl = listCategoriesEl[0].querySelectorAll('.item ul li');
+// console.log('Elements:', itemsCategotyEl.length);
+
+function categories (el) {
+    console.log('Category:', el.firstElementChild.textContent);
+    console.log('Elements:', el.querySelectorAll('.item ul li').length);
+ };
+allCategoriesEl.forEach(categories);

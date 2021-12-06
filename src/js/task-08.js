057bbac09048b0b7735e300ac4fae0f1fae56801
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-06 22:55:00 +0200

    task-08 finish

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector('.login-form');
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const options = {
+        email: event.currentTarget.elements.email.value.trim(),
+        password: event.currentTarget.elements.password.value.trim()
+    }
+    if (!options.email || !options.password) {
+        return alert('Please fill in all the fields!')
+    };
+    console.log(options);
+    formEl.reset();
+}
+
+formEl.addEventListener('submit', handleSubmit);

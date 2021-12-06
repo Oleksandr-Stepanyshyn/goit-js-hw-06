83839bc259e2a3ab5cc95b911e1606eb1607c9c7
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-06 23:30:35 +0200

    task-09 finish

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const colorValueEl = document.querySelector('.color');
+const bodyEl = document.body;
+
+const setBodyColor = (event) => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorValueEl.textContent = bodyEl.style.backgroundColor;
+};
+
+buttonEl.addEventListener('click', setBodyColor);

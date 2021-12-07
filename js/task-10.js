0c1d9cf3c30defff8cf9b41de77998275f41522f
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-07 11:54:40 +0200

    finifhed all tasks

--- js/task-10.js
+++ js/task-10.js
@@ -2,26 +2,34 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let count;
+const inputValueEl = document.querySelector('#controls input');
+const createBtnEl = inputValueEl.nextElementSibling;
+const destroyBtnEl = createBtnEl.nextElementSibling;
+const boxesEl = document.querySelector('#boxes');
+
+inputValueEl.addEventListener('blur', onCounterBoxesInput);
+createBtnEl.addEventListener('click', onCreateBtnClick);
+destroyBtnEl.addEventListener('click', destroyBoxes);
+
+// ================= Solution 1 =============== //
 
 function createBoxes(amount) {
   const arrItems = [];
 
   for (let i = 1; i <= amount; i += 1) {
-    const item = document.createElement('div');
     const sizeBox = 20 + 10 * i;
-    item.style.width = `${sizeBox}px`;
-    item.style.height = `${sizeBox}px`;
-    item.style.backgroundColor = getRandomHexColor();
+    const backgroundColor = getRandomHexColor();
+    const item = `<div style="background-color: ${backgroundColor}; width: ${sizeBox}px; height: ${sizeBox}px;"></div>`;
     arrItems.push(item);
   }
-
-  boxesEl.append(...arrItems);
+  
+  boxesEl.innerHTML = arrItems.join("");
 };
+
+let count;
 
 function onCounterBoxesInput(event) {
   count = event.currentTarget.value;
-  console.log(count);
   return count;
 };
 
@@ -33,11 +41,38 @@ function destroyBoxes() {
   boxesEl.innerHTML = "";
 };
 
-const inputValueEl = document.querySelector('#controls input');
-const createBtnEl = inputValueEl.nextElementSibling;
-const destroyBtnEl = createBtnEl.nextElementSibling;
-const boxesEl = document.querySelector('#boxes');
 
-inputValueEl.addEventListener('blur', onCounterBoxesInput);
-createBtnEl.addEventListener('click', onCreateBtnClick);
-destroyBtnEl.addEventListener('click', destroyBoxes);
+// ================= Solution 2 =============== //
+
+// let count;
+
+// function createBoxes(amount) {
+//   const arrItems = [];
+
+//   for (let i = 1; i <= amount; i += 1) {
+//     const item = document.createElement('div');
+//     const sizeBox = 20 + 10 * i;
+//     item.style.width = `${sizeBox}px`;
+//     item.style.height = `${sizeBox}px`;
+//     item.style.backgroundColor = getRandomHexColor();
+//     arrItems.push(item);
+//   }
+
+//   boxesEl.append(...arrItems);
+// };
+
+// function onCounterBoxesInput(event) {
+//   count = event.currentTarget.value;
+//   console.log(count);
+//   return count;
+// };
+
+// function onCreateBtnClick(event) {
+//   createBoxes(count);
+// };
+
+// function destroyBoxes() {
+//   boxesEl.innerHTML = "";
+// };
+
+
